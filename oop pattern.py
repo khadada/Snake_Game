44828481a11e2e8ae98883a67e4a8234b6af1d38
khadada <lkhadada@gmail.com>
2021-11-13 09:51:04 +0100

    I update control to the snake direction

--- oop pattern.py
+++ oop pattern.py
@@ -6,10 +6,15 @@ screen = Screen()
 screen.setup(width=400, height=300)
 screen.bgcolor('black')
 screen.title('Snake Game')
-screen.listen()
+
 screen.tracer(0)    # stop tracer until the snake is ready
 snake = Snake()
 game_is_on = True
+screen.listen()
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
 while game_is_on:
     screen.update()      # because all segments in their position we update the screen
     time.sleep(0.5)      # here to wait the all segment take its new position
--- snake.py
+++ snake.py
@@ -1,12 +1,17 @@
 from turtle import Turtle
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_STEP_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.setup()
+        self.head = self.segments[0]
 
     def setup(self):
         for position in STARTING_POSITION:
@@ -23,3 +28,19 @@ class Snake:
             new_y = self.segments[num_seg - 1].ycor()
             self.segments[num_seg].goto(new_x, new_y)
         self.segments[0].forward(MOVE_STEP_DISTANCE)
+    def up(self):
+        global UP
+        if self.head.heading() != DOWN:
+             self.head.setheading(UP)
+    def down(self):
+        global DOWN
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def left(self):
+        global LEFT
+        if self.head.heading() != RIGHT:
+           self.head.setheading(LEFT)
+    def right(self):
+        global RIGHT
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
